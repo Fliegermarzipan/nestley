d5182ea99e9d1095173c506e21ca50f8a0ba057f
Robin Broda <robin@broda.me>
2018-12-01 23:08:54 +0100

    Add Color enum

---
+++ src/color.cr
@@ -0,0 +1,10 @@
+module NEStley
+  enum Color
+    LIGHT   = 0xffffff
+    DARK    = 0x212529
+    PRIMARY = 0x92cc41
+    SUCCESS = 0x209cee
+    WARNING = 0xf7d51d
+    ERROR   = 0xe76e55
+  end
+end
