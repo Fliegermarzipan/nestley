ebee1021c8c5bf355ad5197cdfe01a4ce90cf4e7
Robin Broda <robin@broda.me>
2018-12-01 23:11:36 +0100

    Fix typo in BorderedOpaqueContainer

--- src/entity/borderedopaquecontainer.cr
+++ src/entity/borderedopaquecontainer.cr
@@ -1,7 +1,7 @@
 require "./opaquecontainer"
 
 module NEStley
-  class BorderedOpaqueContainre < OpaqueContainer
+  class BorderedOpaqueContainer < OpaqueContainer
     def initialize(@width, @height, @x = 0, @y = 0, @background_color = 0xaaaa00)
       super(@width, @height, @x, @y, @background_color)
       @entities << Border.new(@width, @height, 0, 0, NEStley::Color::LIGHT.value)
