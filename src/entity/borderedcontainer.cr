3238e2c732d12c63dad510210af4ddd70191d377
Robin Broda <robin@broda.me>
2018-12-06 19:59:34 +0100

    Add BorderedContainer

---
+++ src/entity/borderedcontainer.cr
@@ -0,0 +1,11 @@
+require "./container"
+require "./border"
+
+module NEStley
+  class BorderedContainer < Container
+    def initialize(@width, @height, @x = 0, @y = 0, @background_color = 0xaaaa00, @border_color = 0xaaaa00, rounded = true)
+      super(@width, @height, @x, @y, @background_color)
+      @entities << Border.new(@width, @height, 0, 0, @border_color, rounded)
+    end
+  end
+end
