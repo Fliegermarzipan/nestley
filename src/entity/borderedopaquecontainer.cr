84ae835e1dcbf0d5a5b753958b5588d4920f5bf3
Robin Broda <robin@broda.me>
2018-12-06 19:59:06 +0100

    BorderedOpaqueContainer: use new border format

--- src/entity/borderedopaquecontainer.cr
+++ src/entity/borderedopaquecontainer.cr
@@ -2,9 +2,9 @@ require "./opaquecontainer"
 
 module NEStley
   class BorderedOpaqueContainer < OpaqueContainer
-    def initialize(@width, @height, @x = 0, @y = 0, @background_color = 0xaaaa00)
+    def initialize(@width, @height, @x = 0, @y = 0, @background_color = 0xaaaa00, @border_color = 0xaaaa00, rounded = true)
       super(@width, @height, @x, @y, @background_color)
-      @entities << Border.new(@width, @height, 0, 0, NEStley::Color::LIGHT.value)
+      @entities << Border.new(@width, @height, 0, 0, @border_color, rounded)
     end
   end
 end
