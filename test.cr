1a0b3ce7493ba4e434ed5e0f682d4933781cd78c
Robin Broda <robin@broda.me>
2018-12-06 20:00:03 +0100

    test: use new components

--- test.cr
+++ test.cr
@@ -13,10 +13,11 @@ img = Crixel::SixelImage.new(width, height)
 entities = [] of NEStley::Entity
 entities << NEStley::Rect.new(200_u32, 100_u32, 50, 50, NEStley::Color::PRIMARY.value)
 
-cont = NEStley::BorderedOpaqueContainer.new(400_u32, 300_u32, (width/2 - 200).to_i32, (height/2 - 150).to_i32, NEStley::Color::DARK.value)
+cont = NEStley::BorderedOpaqueContainer.new(400_u32, 300_u32, (width/2 - 200).to_i32, (height/2 - 150).to_i32, NEStley::Color::LIGHT.value, NEStley::Color::DARK.value)
 entities << cont
 
-# cont << NEStley::Rect.new(400_u32, 300_u32, 0, 0, NEStley::Color::DARK.value)
+cont << NEStley::Button.new(130_u32, 60_u32, 30, 200, NEStley::Color::SUCCESS.value)
+cont << NEStley::Button.new(130_u32, 60_u32, 240, 200, NEStley::Color::ERROR.value)
 
 height.times do |y|
   width.times do |x|
@@ -29,7 +30,7 @@ height.times do |y|
         end
       end
     end
-    img.set(x, y, NEStley::Color::DARK.value.to_u32) unless drawn
+    img.set(x, y, 0xffffff_u32) unless drawn
   end
 end
 entities.each do |entity|
